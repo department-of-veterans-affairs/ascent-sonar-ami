4878fac43c9d1825233dc4ac42d7431ea779a43a
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-05-17 08:10:51 -0500

    PLAT-174: Add set -e to all scripts; add debug statements to scripts. Put associate ip in sonarqube.json

--- packer/install-java.sh
+++ packer/install-java.sh
@@ -2,7 +2,7 @@
 set -e
 
 JAVA_FILE=jdk1.8.0_171
-
+echo "------- Installing java"
 # Install Java Runtime
 curl -L -H "Cookie: oraclelicense=accept-securebackup-cookie" -o $JAVA_FILE.tar.gz "http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/jdk-8u171-linux-x64.tar.gz"
 tar -xzf $JAVA_FILE.tar.gz
@@ -12,3 +12,6 @@ sudo mv $JAVA_FILE /usr/lib/jvm/$JAVA_FILE
 sudo chown -R root:root /usr/lib/jvm/$JAVA_FILE
 
 sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/$JAVA_FILE/bin/java 1200
+echo "-------- Finished!"
+echo ""
+echo ""
--- packer/install-sonar.sh
+++ packer/install-sonar.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
+set -e
+echo "------ Installing sonar"
 sudo wget -O /etc/yum.repos.d/sonar.repo http://downloads.sourceforge.net/project/sonar-pkg/rpm/sonar.repo
 sudo yum install -y --quiet sonar
+echo "------ Finished!"
