d9fcebb78d3747b4f92e76b3c06efa986193d10f
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-05-17 13:30:06 -0500

    PLAT-174: add permissions to run-sonarqube

--- packer/install-sonar.sh
+++ packer/install-sonar.sh
@@ -3,4 +3,5 @@ set -e
 echo "------ Installing sonar"
 sudo wget -O /etc/yum.repos.d/sonar.repo http://downloads.sourceforge.net/project/sonar-pkg/rpm/sonar.repo
 sudo yum install -y --quiet sonar
+sudo chmod 755 /home/ec2-user/run-sonarqube.sh
 echo "------ Finished!"
