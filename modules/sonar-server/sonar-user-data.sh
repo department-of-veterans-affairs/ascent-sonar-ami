e06b0197b5ec9aee4384b8369d8fe60f9a41bf48
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-05-17 13:49:35 -0500

    PLAT-174: firx template variables

--- modules/sonar-server/sonar-user-data.sh
+++ modules/sonar-server/sonar-user-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # The following variables are replaced with terraform interpolation
-/home/ec2-user/run-sonarqube.sh ${db_username} \
-                                ${db_password} \
+/home/ec2-user/run-sonarqube.sh ${sonar_db_username} \
+                                ${sonar_db_password} \
                                 ${jdbc_url} > /home/ec2-user/run-sonarqube.out 2>&1
