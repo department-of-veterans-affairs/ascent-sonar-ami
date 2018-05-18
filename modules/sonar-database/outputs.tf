d8b9851cedb99a9bf16ace0c7c6bfa321e620ffc
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-05-18 08:29:21 -0500

    PLAT-174: add the sonar database security group to the inbound rules for sonar instance

--- modules/sonar-database/outputs.tf
+++ modules/sonar-database/outputs.tf
@@ -5,3 +5,7 @@ output "port" {
 output "endpoint" {
   value = "${aws_db_instance.sonar_database_instance.endpoint}"
 }
+
+output "security_group_id" {
+  value = "${aws_security_group.sonar_database_security_group.id}"
+}
