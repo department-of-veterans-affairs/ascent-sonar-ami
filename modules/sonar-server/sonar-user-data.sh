195becdd490fe63f8d80ade446cf25aa6f865ef2
jasonluck <jluck@meetveracity.com>
2018-06-26 10:55:34 -0400

    quoting password as some value cause script to fail

--- modules/sonar-server/sonar-user-data.sh
+++ modules/sonar-server/sonar-user-data.sh
@@ -2,6 +2,6 @@
 
 # The following variables are replaced with terraform interpolation
 /home/ec2-user/run-sonarqube.sh ${sonar_db_username} \
-                                ${sonar_db_password} \
+                                '${sonar_db_password}' \
                                 ${jdbc_url} \
                                 ${sonar_port} > /home/ec2-user/run-sonarqube.out 2>&1
