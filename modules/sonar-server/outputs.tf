b8b6b5377d57dde133a222dcbe3aa284f638cfce
jasonluck <jluck@meetveracity.com>
2018-07-04 09:33:18 -0400

    output the security id

--- modules/sonar-server/outputs.tf
+++ modules/sonar-server/outputs.tf
@@ -5,3 +5,7 @@ output "sonar_private_ip" {
 output "sonar_port" {
   value = "${module.security_group_rules.sonar_port}"
 }
+
+output "security_group_id" {
+  value = "${aws_security_group.sonar_security_group.id}"
+}
